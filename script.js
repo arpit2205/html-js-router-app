b8ee5dd8a1aba86a2b298da48fdca8239d10debd
Arpit Gupta <arpit.8.a@gmail.com>
2023-01-12 22:46:40 +0530

    Form validations on UI, store all signed-up user data, safe routing for login/signup

--- script.js
+++ script.js
@@ -5,6 +5,25 @@ const routes = {
   404: "/views/404.html",
 };
 
+const toggleLogoutButton = function (action) {
+  let logoutButton = document.getElementById("logout-btn");
+  logoutButton.style.display = action === "hide" ? "none" : "block";
+};
+
+const toggleLoginLinks = function (action) {
+  let signupLink = document.getElementById("signup-link");
+  let loginLink = document.getElementById("login-link");
+  signupLink.style.display = action === "hide" ? "none" : "block";
+  loginLink.style.display = action === "hide" ? "none" : "block";
+};
+
+const setError = function (msg) {
+  let errorField = document.getElementById("error-field");
+
+  errorField.style.display = msg === "" ? "none" : "block";
+  errorField.innerHTML = "Error: " + msg;
+};
+
 // Router
 const route = function (event) {
   event = event || window.event;
@@ -23,12 +42,26 @@ const redirectTo = async function (path) {
 // Function to render html pages based on route
 const handleLocation = async function () {
   const path = window.location.pathname;
+  const authUser = JSON.parse(localStorage.getItem("auth-user"));
 
   // pvt route "/"
   if (path === "/") {
-    const user = JSON.parse(localStorage.getItem("user"));
-    if (!user || user.isLoggedIn === false) {
+    // check if a user is logged in
+    if (!authUser) {
       redirectTo("/login");
+      toggleLogoutButton("hide");
+      toggleLoginLinks("show");
+      return;
+    } else {
+      toggleLogoutButton("show");
+      toggleLoginLinks("hide");
+    }
+  }
+
+  // prevent login/signup when user is logged in
+  if (path === "/login" || path === "/signup") {
+    if (authUser) {
+      redirectTo("/");
       return;
     }
   }
@@ -51,40 +84,61 @@ const handleSignup = function () {
 
   // empty fields validation
   if (!username || !email || !pwd) {
-    alert("Enter all fields");
+    setError("Enter all fields");
     return;
   }
 
   // email validation
   if (!email.match(/\S+@\S+\.\S+/)) {
-    alert("Enter a valid email");
+    setError("Enter a valid email");
     return;
   }
 
   // username length validation
   if (username.length < 6) {
-    alert("Username must contain at least 6 characters");
+    setError("Username must contain at least 6 characters");
     return;
   }
 
   // password length validation
   if (pwd.length < 8) {
-    alert("Password must contain at least 8 characters");
+    setError("Password must contain at least 8 characters");
     return;
   }
 
+  const users = JSON.parse(localStorage.getItem("users"));
+
+  // check if email/username already exists
+  if (users) {
+    let found = users.find(
+      (user) =>
+        user.email === email ||
+        user.username === email ||
+        user.email === username ||
+        user.username === username
+    );
+    if (found) {
+      setError("Email/Username already exists");
+      return;
+    }
+  }
+
   const newUser = {
+    id: Date.now(),
     username,
     email,
     pwd,
-    isLoggedIn: false,
   };
 
-  console.log(newUser);
-  localStorage.setItem("user", JSON.stringify(newUser));
+  // add current user to the array of users in localStorage
+  if (users) {
+    localStorage.setItem("users", JSON.stringify([...users, newUser]));
+  } else {
+    localStorage.setItem("users", JSON.stringify([newUser]));
+  }
 
   alert("Signup success!!! You can now proceed to login");
-
+  setError("");
   redirectTo("/login");
 };
 
@@ -92,27 +146,34 @@ const handleLogin = function () {
   const usernameOrEmail = document.getElementById("email-login-input").value;
   const pwd = document.getElementById("password-login-input").value;
 
-  const user = JSON.parse(localStorage.getItem("user"));
+  const users = JSON.parse(localStorage.getItem("users"));
 
-  if (!user) {
-    alert("Please sign up first");
+  if (!users) {
+    setError("Please sign up first");
     return;
   }
 
-  if (
-    (user.email === usernameOrEmail || user.username === usernameOrEmail) &&
-    user.pwd === pwd
-  ) {
+  let found = users.find(
+    (user) =>
+      (user.email === usernameOrEmail || user.username === usernameOrEmail) &&
+      user.pwd === pwd
+  );
+
+  if (found) {
+    setError("");
     alert("Log in successfull!!! You can now go to home");
-    const loggedInUser = { ...user, isLoggedIn: true };
-    localStorage.setItem("user", JSON.stringify(loggedInUser));
+    localStorage.setItem("auth-user", JSON.stringify(found));
     redirectTo("/");
+    toggleLogoutButton("show");
+    toggleLoginLinks("hide");
   } else {
-    alert("Incorrect username/email or password");
+    setError("Incorrect username/email or password");
   }
 };
 
 const handleLogout = function () {
-  localStorage.removeItem("user");
+  localStorage.removeItem("auth-user");
+  toggleLogoutButton("hide");
+  toggleLoginLinks("show");
   redirectTo("/login");
 };
